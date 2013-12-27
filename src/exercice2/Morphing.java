d51e16444d31b52d440bb5d3ee62173509d31089
Katia <Katia@Katia-PC>
2013-12-27 18:22:31 +0100

    Commentaire ajouté

--- src/exercice2/Morphing.java
+++ src/exercice2/Morphing.java
@@ -24,8 +24,8 @@ import java.util.regex.Pattern;
 public class Morphing {
 
     protected String cheminFichierEntre;
-    protected List<String> listeMot;
-    protected int[][] levenshtein;
+    protected List<String> listeMot; // List contenant l'extraction des mots d'un fichier
+    protected int[][] levenshtein; // matrice
     protected int distanceLevenshtein;
 
     /**
@@ -38,12 +38,15 @@ public class Morphing {
     }
 
     /**
-     * Compliqué les REGEX
+     * Methode qui réalise le parsage(extraction) de tous les mots du fichier donné en paramétre du Constructeur
+     * On utilise la classe Scanner, comme Java nous le préconise (StringTokenizer étant inusité)
+     * 
+     * Problème avec les REGEX, donc pour le moment seul le separateur "espace" est pris en compte
      */
     public void parseTexteVersMot() {
         try {
-            Pattern pattern = Pattern.compile("[^\\ \\.\\*\\?\\(\\)\\[\\]\\{\\}\\^\\$\\|\\+]");
-            Scanner scanner = new Scanner(new FileReader(this.cheminFichierEntre));
+            Pattern pattern = Pattern.compile("[^\\ \\.\\*\\?\\(\\)\\[\\]\\{\\}\\^\\$\\|\\+]"); // pattern qui permet avec une regex de récuperer tous les mots entre les différents séparateur
+            Scanner scanner = new Scanner(new FileReader(this.cheminFichierEntre)); // seul le separateur "espace" est pris en compte (par défaut)
             String mot = null;
             while (scanner.hasNext()) {
                 mot = scanner.next();
@@ -56,8 +59,8 @@ public class Morphing {
     }
 
 //    /**
-//     * Pas sur que ce soit vraiment demandé
-//     * Pas utile
+//     * Methode qui ecrit dans un fichier la liste des mots extraits du fichier passé en entrée
+//     * Pas demandé
 //     * @param cheminFichierSorti
 //     */
 //    public void extractionMot(String cheminFichierSorti) {
@@ -76,19 +79,25 @@ public class Morphing {
 //    }
 
     /**
+     * Methode qui calcule la distance de levenshtein entre 2 mots.
+     * Algorithme wikipedia
+     * 
      * Le passage en tableau de caractere n'est pas obligatoire mais je prefere
      * comme cela. On utilise des tableaux, donc autant en faire de même pour
      * nos strings ^^.
-     *
+     * 
      * @param source
      * @param target
-     * @return
+     * @return la distance de levenshtein entre les 2 mots, sous la forme d'un string
      */
     public String distanceLevenshtein(String source, String target) {
         char[] chaineCharSource = source.toCharArray();
         char[] chaineCharTarget = target.toCharArray();
-        int cost = 0;
+        int cost = 0; // cost représente si oui ou non 2 caracteres sont "egaux"
+        
+        // Matrice de taille (chaineCharSource.length + 1) * (chaineCharTarget.length + 1) q3 du TD
         levenshtein = new int[chaineCharSource.length + 1][chaineCharTarget.length + 1];
+        
         for (int i = 0; i < chaineCharSource.length + 1; i++) {
             levenshtein[i][0] = i;
         }
@@ -97,21 +106,28 @@ public class Morphing {
         }
 
         // on commence ici à 1 afin d'éviter une exception d'index : 0-1 = -1 --> erreur.
-        // C'est pour cela qu'on pose i-1 et j-1 dans la 1ere condition
+        // C'est pour cela qu'on pose i-1 et j-1 dans la condition
         for (int i = 1; i < chaineCharSource.length + 1; i++) {
             for (int j = 1; j < chaineCharTarget.length + 1; j++) {
                 if (chaineCharSource[i - 1] == chaineCharTarget[j - 1]) {
-                    cost = 0;
+                    cost = 0; // pas de coût puisque les 2 caracteres sont egaux
                 } else {
-                    cost = 1;
+                    cost = 1; // coût puisque les 2 caracteres sont différents
                 }
+                // Correspond a la formule de récurrence de la distance de levenshtein q2 du TD
                 levenshtein[i][j] = minimum((levenshtein[i - 1][j] + 1), (levenshtein[i][j - 1] + 1), (levenshtein[i - 1][j - 1] + cost));
             }
-
         }
         return "<" + source + ", " + target + ">, la distance de Levenshtein = " + levenshtein[chaineCharSource.length][chaineCharTarget.length];
     }
 
+    /**
+     * 
+     * @param a
+     * @param b
+     * @param c
+     * @return minimum des 3 valeurs
+     */
     public int minimum(int a, int b, int c) {
         return Math.min(Math.min(a, b), c);
     }

e7823731eca511896e34abd4fe9b2483163b1845
nikolai <nikolai@NIKOLAI-MINT>
2013-12-27 17:40:43 +0100

    commit avant changement de ROOT en lui même

--- src/exercice1/UnionFind.java
+++ src/exercice1/UnionFind.java
@@ -8,8 +8,10 @@ package exercice1;
 import java.util.ArrayList;
 
 /**
- * http://en.wikipedia.org/wiki/Disjoint-set_data_structure
- * http://fr.wikipedia.org/wiki/Union-Find
+ *
+ * Utilisation du type de recherche : Path Compression
+ *
+ * cf http://en.wikipedia.org/wiki/Disjoint-set_data_structure
  *
  * @author CARRARA Nicolas
  * @param <E>
@@ -24,6 +26,11 @@ public class UnionFind<E> {
 
     private final NoeudUF<E> MISSING = new NoeudUF(null, null, -1);
     private final NoeudUF<E> ROOT = new NoeudUF(null, null, -2);
+
+    /**
+     * type de recherche
+     */
+    public final static int COMPRESSION = 1;
 
     /**
      * Un noeud d'un arbre de la structure UnionFind
@@ -76,7 +83,7 @@ public class UnionFind<E> {
      * @throws exercice1.Exercice1Exception
      */
     public void makeset(E data) throws Exercice1Exception {
-        if(data ==null){
+        if (data == null) {
             throw new Exercice1Exception("La donnée ne peut être null");
         }
         if (getNodeOfData(data) == MISSING) {
@@ -94,7 +101,7 @@ public class UnionFind<E> {
      * @throws exercice1.Exercice1Exception
      */
     public E find(E data) throws Exercice1Exception {
-        if(data ==null){
+        if (data == null) {
             throw new Exercice1Exception("La donnée ne peut être null");
         }
         NoeudUF<E> result = getNodeOfData(data);
@@ -108,6 +115,54 @@ public class UnionFind<E> {
     }
 
     /**
+     * Cherche le noeud de donnée data puis remonte de père en père jusqu'a
+     * retrouver le noeud racine de l'arbre. Applique l'ago de typeRecherche sur
+     * x
+     *
+     * @param x
+     * @param typeRecherche
+     * @return la valeur de la racine de l'arbre qui contient l'element data,
+     * null si cet element n'existe pas
+     * @throws Exercice1Exception
+     */
+    public E find(E x, int typeRecherche) throws Exercice1Exception {
+        if (x == null) {
+            throw new Exercice1Exception("La donnée ne peut être null");
+        }
+        NoeudUF<E> nodeX = getNodeOfData(x);
+        E result = null;
+        if (nodeX != MISSING) {
+            switch (typeRecherche) {
+                case COMPRESSION:
+                    result = findCompression(nodeX).data;
+                    break;
+                default:
+                    throw new Exercice1Exception("Type de recherche inconnu");
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Cherche le noeud root correpondant à nodeX. Applique l'algorithme de
+     * compression pendant la remonté de l'arbre.
+     *
+     * @param nodeX
+     * @return
+     * @throws Exercice1Exception
+     */
+    private NoeudUF<E> findCompression(NoeudUF<E> nodeX) throws Exercice1Exception {
+        NoeudUF<E> result = nodeX.pere;
+
+        if (nodeX.pere != ROOT) {
+            nodeX.pere = findCompression(nodeX.pere);
+        } else {
+            result = nodeX;
+        }
+        return nodeX.pere;
+    }
+
+    /**
      * fusionne deux ensembles, le root des deux arbres fusionés est celui ayant
      * la plus grande priorité des deux anciens root
      *
@@ -117,12 +172,44 @@ public class UnionFind<E> {
      * @throws exercice1.Exercice1Exception
      */
     public E union(E x, E y) throws Exercice1Exception {
-        if(x ==null || y == null){
-            throw new Exercice1Exception("x et y ne peut=vent être null");
+        if (x == null || y == null) {
+            throw new Exercice1Exception("x et y ne peuvent être null");
         }
         E root = null;
         E rootX = find(x);
         E rootY = find(y);
+        if (rootX != null && rootY != null) {
+            NoeudUF<E> nodeRootX = getNodeOfData(rootX);
+            NoeudUF<E> nodeRootY = getNodeOfData(rootY);
+            if (nodeRootX.priorite > nodeRootY.priorite) {
+                nodeRootY.pere = nodeRootX;
+                root = nodeRootX.data;
+            } else {
+                nodeRootX.pere = nodeRootY;
+                root = nodeRootY.data;
+            }
+        }
+        return root;
+    }
+
+    /**
+     * fusionne deux ensembles, le root des deux arbres fusionés est celui ayant
+     * la plus grande priorité des deux anciens root. Applique l'algo de
+     * typeRecherche sur x et y
+     *
+     * @param x
+     * @param y
+     * @param typeRecherche
+     * @return le nouveau root, null si x ou y n'existe pas.
+     * @throws Exercice1Exception
+     */
+    public E union(E x, E y, int typeRecherche) throws Exercice1Exception {
+        if (x == null || y == null) {
+            throw new Exercice1Exception("x et y ne peuvent être null");
+        }
+        E root = null;
+        E rootX = find(x, typeRecherche);
+        E rootY = find(y, typeRecherche);
         if (rootX != null && rootY != null) {
             NoeudUF<E> nodeRootX = getNodeOfData(rootX);
             NoeudUF<E> nodeRootY = getNodeOfData(rootY);
--- test/exercice1/VerificationUnionFind.java
+++ test/exercice1/VerificationUnionFind.java
@@ -21,9 +21,40 @@ public class VerificationUnionFind {
     public static void main(String[] args){
         try {
             UnionFind<Integer> uf = new UnionFind<>();
-            System.out.println("uf : "+uf);
+//            uf.makeset(1);
+//            uf.makeset(3);
+//            uf.makeset(4);
+//            uf.makeset(7);
+//            uf.makeset(2);
+//            uf.makeset(5);
+//            uf.makeset(6);
+//            uf.makeset(8);
+//            
+//            System.out.println("uf : "+uf);
+//            uf.makeset(8);
+//            System.out.println("uf : "+uf);
+//            System.out.println("uf.find(8) : "+uf.find(8));
+//            System.out.println("uf.find(10) : "+uf.find(10));
+//            
+//            System.out.println("uf.union(1, 3) : "+uf.union(1, 3));
+//            System.out.println("uf : "+uf);
+//            System.out.println("uf.union(3, 4) : "+uf.union(3, 4));
+//            System.out.println("uf : "+uf);
+//            System.out.println("uf.union(7, 3) : "+uf.union(7, 3));
+//            System.out.println("uf : "+uf);
+//            uf.union(2, 6);
+//            uf.union(2, 5);
+//            uf.union(8, 5);
+//            System.out.println("uf : "+uf);
+//            
+//            uf.union(5, 7);
+//            System.out.println("uf : "+uf);
+//            
+//            System.out.println("uf.union(5, null) : "+uf.union(5, null));
+            
+            // test compression
+            uf = new UnionFind<>();
             uf.makeset(1);
-            System.out.println("uf : "+uf);
             uf.makeset(3);
             uf.makeset(4);
             uf.makeset(7);
@@ -31,28 +62,26 @@ public class VerificationUnionFind {
             uf.makeset(5);
             uf.makeset(6);
             uf.makeset(8);
-            
+//            System.out.println("uf : "+uf);
+//            uf.makeset(8);
+//            System.out.println("uf : "+uf);
+//            System.out.println("uf.find(8) : "+uf.find(8,UnionFind.COMPRESSION));
+//            System.out.println("uf.find(10) : "+uf.find(10,UnionFind.COMPRESSION));
+//            
+            System.out.println("uf.union(1, 3) : "+uf.union(1, 3,UnionFind.COMPRESSION));
             System.out.println("uf : "+uf);
-            uf.makeset(8);
+            System.out.println("uf.union(3, 4) : "+uf.union(3, 4,UnionFind.COMPRESSION));
             System.out.println("uf : "+uf);
-            System.out.println("uf.find(8) : "+uf.find(8));
-            System.out.println("uf.find(10) : "+uf.find(10));
-            
-            System.out.println("uf.union(1, 3) : "+uf.union(1, 3));
-            System.out.println("uf : "+uf);
-            System.out.println("uf.union(3, 4) : "+uf.union(3, 4));
-            System.out.println("uf : "+uf);
-            System.out.println("uf.union(7, 3) : "+uf.union(7, 3));
-            System.out.println("uf : "+uf);
-            uf.union(2, 6);
-            uf.union(2, 5);
-            uf.union(8, 5);
-            System.out.println("uf : "+uf);
-            
-            uf.union(5, 7);
-            System.out.println("uf : "+uf);
-            
-            System.out.println("uf.union(5, null) : "+uf.union(5, null));
+//            System.out.println("uf.union(7, 3) : "+uf.union(7, 3,UnionFind.COMPRESSION));
+//            System.out.println("uf : "+uf);
+//            uf.union(2, 6,UnionFind.COMPRESSION);
+//            uf.union(2, 5,UnionFind.COMPRESSION);
+//            uf.union(8, 5,UnionFind.COMPRESSION);
+//            System.out.println("uf : "+uf);
+//            
+//            uf.union(5, 7,UnionFind.COMPRESSION);
+//            System.out.println("uf : "+uf);
+//            
             System.out.println("Flag end test");
         } catch (Exercice1Exception ex) {
             Logger.getLogger(VerificationUnionFind.class.getName()).log(Level.SEVERE, null, ex);

fbe6299bcf9bf1deee82200135043f87b59706e4
Katia <Katia@Katia-PC>
2014-01-02 00:27:28 +0100

    MAJ : Auteurs, formatage et des "sout" inutiles

--- test/exercice1/VerificationGenerale.java
+++ test/exercice1/VerificationGenerale.java
@@ -19,75 +19,70 @@ public class VerificationGenerale {
      */
     public static void main(String[] args) {
         try {
+            System.out.println("\n******************ABC******************************\n");
             // ABC
             ArbreBinaireCartesien<Integer> abc = new ArbreBinaireCartesien<>();
             abc.insererClef(11, 15);
             abc.insererClef(6, 2);
-//            abc.insererClef(4, 3);
-//            abc.insererClef(1, 2);
-//            abc.insererClef(5, 8);
-//            abc.insererClef(7, 1);
-//            abc.insererClef(9, 4);
-//            abc.insererClef(8, 9);
+            abc.insererClef(4, 3);
+            abc.insererClef(1, 0);
+            abc.insererClef(5, 8);
+            abc.insererClef(7, 1);
+            abc.insererClef(9, 4);
+            abc.insererClef(8, 9);
             abc.insererClef(15, 12);
-//            abc.insererClef(12, 6);
-//            abc.insererClef(16, 5);
+            abc.insererClef(12, 6);
+            abc.insererClef(16, 5);
             System.out.println(abc.computeHauteur() + " / " + abc);
-//            System.out.println("Fin : " + abc.remove(8));
-//            abc.remove(8);
-            abc.supprimer(11);
+            int i;
+            i = 8;
+            abc.supprimer(i);
+            System.out.println("Suppression de " + i + " : ");
             System.out.println(abc.computeHauteur() + " / " + abc);
 
-//            abc.insererClef(20, 30);
-//            System.out.println(abc.computeHauteur() + " / " + abc);
-//            abc.insererClef(12, 10);
-//            System.out.println(abc.computeHauteur() + " / " + abc);
-//            System.out.println("Noeud : " + abc.rechercheClef(1, ArbreBinaireCartesien.SEARCH_LIST));
-//            System.out.println("Noeud");
-//            for (int i = 0; i < abc.listNoeudArbres.size(); i++) {
-//                System.out.println(abc.listNoeudArbres.get(i));
-//            }
+            System.out.println("\n******************TREAP******************************\n");
             // Treap
-//            Treap<Integer> treap = new Treap<>();
-//            treap.insererClefTreap(3);
-//            System.out.println(treap.computeHauteur() + " / " + treap);
-//            treap.insererClefTreap(30);
-//            System.out.println(treap.computeHauteur() + " / " + treap);
-//            treap.insererClefTreap(1);
-//            System.out.println(treap.computeHauteur() + " / " + treap);
-//            treap.insererClefTreap(6);
-//            System.out.println(treap.computeHauteur() + " / " + treap);
-//            treap.insererClefTreap(4);
-//            System.out.println(treap.computeHauteur() + " / " + treap);
-//            System.out.println("Noeud : " + treap.rechercheClef(2));
-//            System.out.println("Noeud");
-//            for (int i = 0; i < abc.listNoeudArbres.size(); i++) {
-//                System.out.println(abc.listNoeudArbres.get(i));
-//            }
-//
-//            // UnionFind
-//            UnionFind<Integer> unionFind = new UnionFind<>();
-//            unionFind.makeset(10);
-//            System.out.println(unionFind);
-//            unionFind.makeset(40);
-//            System.out.println(unionFind);
-//            unionFind.makeset(12);
-//            System.out.println(unionFind);
-//            unionFind.makeset(4);
-//            System.out.println(unionFind);
-//            //unionFind.union(10, 40);
-//            System.out.println(unionFind.union(10, 40));
-//            System.out.println(unionFind.union(40, 12));
-//            System.out.println(unionFind.find(10));
-//            System.out.println(unionFind.find(40));
-//            System.out.println(unionFind.find(12));
-//
-//            System.out.println("Compression");
-//            System.out.println(unionFind.union(10, 40, 1));
-//            System.out.println(unionFind.union(40, 12, 1));
-//            System.out.println(unionFind.find(10, 1));
-//            System.out.println(unionFind.find(40, 1));
-//            System.out.println(unionFind.find(12, 1));
+            Treap<Integer> treap = new Treap<>();
+            treap.insererClefTreap(3);
+            System.out.println(treap.computeHauteur() + " / " + treap);
+            treap.insererClefTreap(30);
+            System.out.println(treap.computeHauteur() + " / " + treap);
+            treap.insererClefTreap(1);
+            System.out.println(treap.computeHauteur() + " / " + treap);
+            treap.insererClefTreap(6);
+            System.out.println(treap.computeHauteur() + " / " + treap);
+            treap.insererClefTreap(4);
+            System.out.println(treap.computeHauteur() + " / " + treap);
+            i = 1;
+            treap.supprimer(i);
+            System.out.println("Suppression de " + i + " : ");
+            System.out.println(treap.computeHauteur() + " / " + treap);
+
+            System.out.println("\n******************UNION FIND******************************\n");
+            // UnionFind
+            UnionFind<Integer> unionFind = new UnionFind<>();
+            unionFind.makeset(10);
+            System.out.println(unionFind);
+            unionFind.makeset(40);
+            System.out.println(unionFind);
+            unionFind.makeset(12);
+            System.out.println(unionFind);
+            unionFind.makeset(4);
+            UnionFind u = unionFind;
+            System.out.println(unionFind);
+            System.out.println("SANS l'algo \"Compression\" :");
+            System.out.println(unionFind.union(10, 40));
+            System.out.println(unionFind.union(40, 12));
+            System.out.println(unionFind.find(10));
+            System.out.println(unionFind.find(40));
+            System.out.println(unionFind.find(12));
+
+            System.out.println("AVEC l'algo \"Compression\" :");
+            System.out.println(u.union(10, 40, 1));
+            System.out.println(u.union(40, 12, 1));
+            System.out.println(u.find(10, 1));
+            System.out.println(u.find(40, 1));
+            System.out.println(u.find(12, 1));
         } catch (Exercice1Exception ex) {
             Logger.getLogger(VerificationGenerale.class.getName()).log(Level.SEVERE, null, ex);
         }
